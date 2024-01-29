32c7a443960ae2caea48ec27542b7dbdd43066f8
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-01-30 00:02:26 +0600

    create navbar

--- src/Components/HomePageComponents/Bannar.jsx
+++ src/Components/HomePageComponents/Bannar.jsx
@@ -11,15 +11,15 @@ const Bannar = () => {
                     <h1 className=' text-yellow-300 text-6xl font-semibold'>Today's Special Pizza Menu</h1>
                     <p className=' text-xl text-gray-200'>Maecenas tempus, tellus eget condimentum rhoncus,<br/> sem quam semper libero, sit amet sem neque sed ipsum.</p>
                     <div className=' flex items-center space-x-3 pt-8'>
-                        <button className=' btn px-8 bg-yellow-300 border-none text-lg '>ORDER NOW</button>
+                        <button className=' btn rounded-sm px-8 hover:bg-yellow-500 bg-yellow-300 border-none text-base'>ORDER NOW</button>
                         <h1 className=' text-5xl font-bold font-custom tracking-wider'>$399</h1>
                         <small className=' font-custom tracking-wider line-through text-yellow-300 text-2xl font-bold'>$500</small>
                     </div>
                 </div>
                 <div className=' relative'>
                     <img className='absolute -top-16 -left-20' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1307-1.png" />
-                    <img className='absolute -bottom-10 right-0 max-w-44' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1206-1.png" />
-                    <img className='absolute -top-0 right-10 w-[80px]' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1207-1.png" />
+                    <img className='absolute -bottom-10 right-10 max-w-44' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1206-1.png" />
+                    <img className='absolute -top-0 right-10 w-[50px]' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1207-1.png" />
                     <img className='absolute -bottom-20 left-10 w-[100px]' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1205-1.png" />
                     <img className=' w-full' src="https://dev.foodotawp.com/wp-content/uploads/2021/08/layer-1203-1.png" alt="" />
                 </div>
--- src/Layouts/AppNavBar.jsx
+++ src/Layouts/AppNavBar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { FaCartShopping, FaTruck } from "react-icons/fa6";
 import './AppNavBar.css'
 
 const AppNavBar = () => {
 
 
 
-
-
     const navLinks = <>
         <li className='text-white'><NavLink to={'/'}>Home</NavLink></li>
-        <li className='text-white'><NavLink to={'/blogs'}>Blogs</NavLink></li>
         <li className='text-white'><NavLink to={'/foods'}>Foods</NavLink></li>
-        <li className='text-white'><NavLink to={'/cart'}>Cart</NavLink></li>
+        <li className='text-white'><NavLink to={'/foods'}>Resturants</NavLink></li>
+        <li className='text-white'><NavLink to={'/blogs'}>Blogs</NavLink></li>
+        <li className='text-white'><NavLink to={'/blogs'}>Dashboard</NavLink></li>
+        <li className='text-white'><NavLink to={'/cart'}>Login</NavLink></li>
     </>
 
 
@@ -33,13 +34,28 @@ const AppNavBar = () => {
                         <img className=' max-w-full rounded-lg' src="https://play-lh.googleusercontent.com/nlassGkKm4bOZtWvw65e3X3K53uodpkM9WuR7951KbkoIGGiNJtrML03zq_sISzagsQ=w240-h480-rw" />
                     </Link>
                 </div>
-                <div className="navbar-end hidden lg:flex">
+                <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {navLinks}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className='text-white mr-5 flex items-center space-x-3 border-r pr-4'>
+                        <Link to={'/'}><FaTruck className=' text-4xl' /></Link>
+                        <div className=' flex flex-col'>
+                            <small className=' text-gray-300'>Delevery Record</small>
+                            <small className=' text-yellow-300 text-center'>+88013210861</small>
+                        </div>
+                    </div>
+                    <div className='text-white mr-5 relative flex items-center space-x-3 '>
+                        <span className='text-xs absolute -top-2 -left-2 rounded-full aspect-square w-5 flex items-center justify-center bg-yellow-300 text-black'>3</span>
+                        <Link to={'/cart'}><FaCartShopping className=' text-2xl' /></Link>
+                        <div className=' flex flex-col'>
+                            <small className=' text-gray-300'>Shopping Cart</small>
+                            <small className=' text-yellow-300 text-center'>$400</small>
+                        </div>
+                    </div>
+                    <button className=' btn px-8 rounded-sm hover:bg-yellow-500 bg-yellow-300 border-none'>DISCOVER FOODS</button>
                 </div>
             </div>
         </nav>
